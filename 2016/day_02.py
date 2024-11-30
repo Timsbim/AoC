a04d781f1961ee5943540276753c0ad9f081cd6f
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-11-30 10:31:18 +0000

    Update year 2016 day 2 full solution

--- 2016/day_02.py
+++ 2016/day_02.py
@@ -5,7 +5,7 @@ from pprint import pprint
 
 
 DAY = 2
-EXAMPLE = True
+EXAMPLE = False
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
@@ -25,12 +25,19 @@ with open(file_name, "r") as file:
     directions = tuple(file.read().splitlines())
 if EXAMPLE:
     pprint(directions)
-    pass
 
 # --------------------------------------------------------------------------- #
 #    Helper                                                                   #
 # --------------------------------------------------------------------------- #
 
+
+def solve(pad, move, directions, start):
+    position, chars = start, []
+    for direction in directions:
+        for instruction in direction:
+            position = move(position, instruction)
+        chars.append(pad[position])
+    return "".join(chars)
 
 
 # --------------------------------------------------------------------------- #
@@ -39,12 +46,30 @@ if EXAMPLE:
 print("Part 1: ", end="")
 
 
-def part_1():
-    return None
+def part_1(directions):
+    PAD = {
+        (0, 2): '1', (1, 2): '2', (2, 2): '3',
+        (0, 1): '4', (1, 1): '5', (2, 1): '6',
+        (0, 0): '7', (1, 0): '8', (2, 0): '9'
+    }
+    
+    
+    def move(position, instruction):
+        x, y = position
+        if instruction == 'U':
+            return x, min(2, y + 1)
+        if instruction == 'D':
+            return x, max(0, y - 1)
+        if instruction == 'R':
+            return min(2, x + 1), y
+        return max(0, x - 1), y
+
+    
+    return solve(PAD, move, directions, (1, 1))
 
 
-print(solution := part_1())
-# assert solution == (if EXAMPLE else)
+print(solution := part_1(directions))
+assert solution == ("1985" if EXAMPLE else "14894")
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
@@ -52,9 +77,31 @@ print(solution := part_1())
 print("Part 2: ", end="")
 
 
-def part_2():
-    return None
+def part_2(directions):
+    PAD = {
+        ( 0,  2): '1',
+        (-1,  1): '2', ( 0,  1): '3', (1,  1): '4',
+        (-2,  0): '5', (-1,  0): '6', (0,  0): '7', (1, 0): '8', (2, 0): '9',
+        (-1, -1): 'A', ( 0, -1): 'B', (1, -1): 'C',
+        ( 0, -2): 'D'
+    }
+    
+    
+    def move(position, instruction):
+        x, y = position
+        if instruction == 'U':
+            position_new = x, y + 1
+        elif instruction == 'D':
+            position_new = x, y - 1
+        elif instruction == 'R':
+            position_new = x + 1, y
+        else:
+            position_new = x - 1, y
+        return position_new if position_new in PAD else position
+
+    
+    return solve(PAD, move, directions, (-2, 0))
 
 
-print(solution := part_2())
-# assert solution == (if EXAMPLE else)
+print(solution := part_2(directions))
+assert solution == ("5DB3" if EXAMPLE else "26B96")
