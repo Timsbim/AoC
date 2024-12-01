ff462ae87155a04b5a49eb3b09f5a0a384d729ff
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-01 11:46:04 +0000

    Update year 2016 day 9 full solution

--- 2016/day_09.py
+++ 2016/day_09.py
@@ -1,60 +1,85 @@
 # --------------------------------------------------------------------------- #
 #    Day 9                                                                    #
 # --------------------------------------------------------------------------- #
+import re
 from pprint import pprint
 
 
 DAY = 9
-EXAMPLE = True
+EXAMPLE = False
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
 # --------------------------------------------------------------------------- #
 print("Day", DAY)
 
-file_name = f"2016/input/day_{DAY:0>2}"
-if EXAMPLE:
-    file_name += "_example"
-file_name += ".txt"
+file_name = f"2016/input/day_{DAY:0>2}.txt"
 
 # --------------------------------------------------------------------------- #
 #    Reading input                                                            #
 # --------------------------------------------------------------------------- #
 
-with open(file_name, "r") as file:
-    pass
-if EXAMPLE:
-    #pprint()
-    pass
+if not EXAMPLE:
+    with open(file_name, "r") as file:
+        compressed = file.read()
 
 # --------------------------------------------------------------------------- #
 #    Helper                                                                   #
 # --------------------------------------------------------------------------- #
 
-
+RE_MARKER = re.compile(r"\((\d+)x(\d+)\)")
+assert compressed.count(" ") == 0
 
 # --------------------------------------------------------------------------- #
 #    Part 1                                                                   #
 # --------------------------------------------------------------------------- #
 print("Part 1: ", end="")
 
+if EXAMPLE:
+    compressed = "X(8x2)(3x3)ABCY"
 
-def part_1():
-    return None
+
+def part_1(compressed):
+    count = 0
+    while m := RE_MARKER.search(compressed):
+        start, end = m.start(), m.end()
+        # Count (non-space) characters in sequence before marker
+        count += start
+        # Count marked length
+        length, multiple = int(m[1]), int(m[2])
+        count += length * multiple
+        # Trim rest
+        compressed = compressed[end+length:]
+    # Count (non-space) characters in end piece and return
+    return count + len(compressed)
 
 
-print(solution := part_1())
-#assert solution == (if EXAMPLE else)
+print(solution := part_1(compressed))
+assert solution == (18 if EXAMPLE else 110346)
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
 # --------------------------------------------------------------------------- #
 print("Part 2: ", end="")
 
+if EXAMPLE:
+    compressed = "(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN"
 
-def part_2():
-    return None
+
+def part_2(compressed):
+    count = 0
+    while m := RE_MARKER.search(compressed):
+        start, end = m.start(), m.end()
+        # Count (non-space) characters in sequence before marker
+        count += start
+        # Count marked length (recursively)
+        length, multiple = int(m[1]), int(m[2])
+        count += multiple * part_2(compressed[end:end+length])
+        # Trim rest
+        compressed = compressed[end+length:]
+    # Count (non-space) characters in end piece and return
+    return count + len(compressed)
 
 
-print(solution := part_2())
-#assert solution == (if EXAMPLE else)
+print(solution := part_2(compressed))
+assert solution == (445 if EXAMPLE else 10774309173)
