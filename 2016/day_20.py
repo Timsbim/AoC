b2a50e02215cf6e005f3881618a7d0e3836137ee
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-08 17:25:44 +0000

    Update year 2016 day 19 full solution

--- 2016/day_20.py
+++ 2016/day_20.py
@@ -1,20 +1,37 @@
 # --------------------------------------------------------------------------- #
-#    Day 19                                                                   #
+#    Day 20                                                                   #
 # --------------------------------------------------------------------------- #
+from pprint import pprint
 
-DAY = 19
-EXAMPLE = False
+
+DAY = 20
+EXAMPLE = True
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
 # --------------------------------------------------------------------------- #
 print("Day", DAY)
 
+file_name = f"2016/input/day_{DAY:0>2}"
+if EXAMPLE:
+    file_name += "_example"
+file_name += ".txt"
+
 # --------------------------------------------------------------------------- #
 #    Reading input                                                            #
 # --------------------------------------------------------------------------- #
 
-ELVES = 5 if EXAMPLE else 3_014_603
+with open(file_name, "r") as file:
+    pass
+if EXAMPLE:
+    #pprint()
+    pass
+
+# --------------------------------------------------------------------------- #
+#    Helper                                                                   #
+# --------------------------------------------------------------------------- #
+
+
 
 # --------------------------------------------------------------------------- #
 #    Part 1                                                                   #
@@ -23,19 +40,11 @@ print("Part 1: ", end="")
 
 
 def part_1():
-    stay = [*range(ELVES)]
-    while (length := len(stay)) > 1:
-        if length % 2:
-            stay_new = [stay[-1]]
-            stay_new.extend(stay[i] for i in range(0, length - 1, 2))
-        else:
-            stay_new = [stay[i]  for i in range(0, length - 1, 2)]
-        stay = stay_new
-    return stay.pop() + 1
+    return None
 
 
 print(solution := part_1())
-assert solution == (3 if EXAMPLE else 1834903)
+#assert solution == (if EXAMPLE else)
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
@@ -44,18 +53,8 @@ print("Part 2: ", end="")
 
 
 def part_2():
-    if ELVES < 3:
-        return 1
-    i, n, delta = 2, 1, 2
-    for i in range(3, ELVES + 1):
-        n += delta
-        if i == ELVES:
-            return n
-        if delta == 1 and n + n == i:
-            delta = 2
-        elif delta == 2 and n == i:
-            delta, n = 1, 0
+    return None
 
 
 print(solution := part_2())
-assert solution == (2 if EXAMPLE else 1420280)
+#assert solution == (if EXAMPLE else)
