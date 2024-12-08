3f9c399452e2327f0cc20bed0cdd8c80952acb4e
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-08 06:23:13 +0000

    Update refactoring year 2024 day 8

--- day_08.py
+++ day_08.py
@@ -1,12 +1,11 @@
 # --------------------------------------------------------------------------- #
-#    Day 8                                                                    #
+#    Day 9                                                                    #
 # --------------------------------------------------------------------------- #
-from itertools import combinations
 from pprint import pprint
 
 
-DAY = 8
-EXAMPLE = False
+DAY = 9
+EXAMPLE = True
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
@@ -22,16 +21,19 @@ file_name += ".txt"
 #    Reading input                                                            #
 # --------------------------------------------------------------------------- #
 
-antennas = {}
 with open(file_name, "r") as file:
-    lines = [line.rstrip() for line in file]
-ROWS, COLS = len(lines), len(lines[0])
-for r, line in enumerate(lines):
-    for c, char in enumerate(line):
-        if char != ".":
-            antennas.setdefault(char, []).append((r, c))
+    #for line in file:
+    pass
+
 if EXAMPLE:
-    pprint(antennas)
+    #pprint()
+    pass
+
+# --------------------------------------------------------------------------- #
+#    Helpers                                                                  #
+# --------------------------------------------------------------------------- #
+
+
 
 # --------------------------------------------------------------------------- #
 #    Part 1                                                                   #
@@ -39,18 +41,12 @@ if EXAMPLE:
 print("Part 1: ", end="")
 
 
-def part_1(antennas):
-    antinodes = set()
-    for positions in antennas.values():
-        for (r1, c1), (r2, c2) in combinations(positions, 2):
-            for r, c in (2 * r1 - r2, 2 * c1 - c2), (2 * r2 - r1, 2 * c2 - c1):
-                if 0 <= r < ROWS and 0 <= c < COLS:
-                    antinodes.add((r, c))
-    return len(antinodes)
+def part_1():
+    return None
 
 
-print(solution := part_1(antennas))
-assert solution == (14 if EXAMPLE else 327)
+print(solution := part_1())
+#assert solution == (if EXAMPLE else)
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
@@ -58,16 +54,9 @@ assert solution == (14 if EXAMPLE else 327)
 print("Part 2: ", end="")
 
 
-def part_2(antennas):
-    antinodes = set()
-    for positions in antennas.values():
-        for (r1, c1), (r2, c2) in combinations(positions, 2):
-            for r, c, dr, dc in (r1, c1, r1 - r2, c1 - c2), (r2, c2, r2 - r1, c2 - c1):
-                while 0 <= r < ROWS and 0 <= c < COLS:
-                    antinodes.add((r, c))
-                    r, c = r + dr, c + dc
-    return len(antinodes)
+def part_2():
+    return None
 
 
-print(solution := part_2(antennas))
-assert solution == (34 if EXAMPLE else 1233)
+print(solution := part_2())
+#assert solution == (if EXAMPLE else)
