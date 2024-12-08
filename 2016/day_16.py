832ad03973deb80f8625862bfbaa7e9a9d98464b
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-08 09:26:22 +0000

    Update year 2016 day 15 full solution

--- 2016/day_16.py
+++ 2016/day_16.py
@@ -1,11 +1,11 @@
 # --------------------------------------------------------------------------- #
-#    Day 15                                                                   #
+#    Day 16                                                                   #
 # --------------------------------------------------------------------------- #
-from math import prod
+from pprint import pprint
 
 
-DAY = 15
-EXAMPLE = False
+DAY = 16
+EXAMPLE = True
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
@@ -22,65 +22,15 @@ file_name += ".txt"
 # --------------------------------------------------------------------------- #
 
 with open(file_name, "r") as file:
-    discs = []
-    for line in file:
-        row = line.split()
-        discs.append((int(row[3]), int(row[-1][:-1])))
-discs = tuple(discs)
+    pass
+if EXAMPLE:
+    #pprint()
+    pass
 
 # --------------------------------------------------------------------------- #
 #    Helper                                                                   #
 # --------------------------------------------------------------------------- #
 
-
-def inv_mod(a, b):
-    if b == 1:
-        return 1
-
-    r0, r1 = a, b
-    s0, s1 = 1, 0
-    while r1 > 0:
-        d, r = divmod(r0, r1)
-        r0, r1 = r1, r
-        s0, s1 = s1, s0 - d * s1
-
-    return s0 if s0 >= 0 else s0 + b
-
-
-def CRT(bs, ns):
-    pns = prod(ns)
-    nps = tuple(pns // n for n in ns)
-
-    return sum(b * np * inv_mod(np, n) for b, np, n in zip(bs, nps, ns)) % pns
-
-
-def solve(discs):
-    """Let
-        n  := number of discs
-        pi := positions of disc i,
-        qi := position of disc i at time = 0,
-        ts := start time
-    
-    then a solution corresponds to the following system of equations:
-    
-        q1 + 1 + ts ≡ 0 (mod p1)
-        q2 + 2 + ts ≡ 0 (mod p2)
-                ...
-        qn + n + ts ≡ 0 (mod pn)
-    
-    i.e.
-    
-        ts ≡ -(q1 + 1) (mod p1)
-        ts ≡ -(q2 + 2) (mod p2)
-                ...
-        ts ≡ -(qn + n) (mod pn)
-    
-    which can be solved by the chinese remainder theorem.
-    """
-    bs = tuple(-(q + n) % p for n, (p, q) in enumerate(discs, 1))
-    ns = tuple(p for p, _ in discs)
-    
-    return CRT(bs, ns)
 
 
 # --------------------------------------------------------------------------- #
@@ -88,13 +38,23 @@ def solve(discs):
 # --------------------------------------------------------------------------- #
 print("Part 1: ", end="")
 
-print(solution := solve(discs))
-assert solution == (5 if EXAMPLE else 121834)
+
+def part_1():
+    return None
+
+
+print(solution := part_1())
+#assert solution == (if EXAMPLE else)
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
 # --------------------------------------------------------------------------- #
 print("Part 2: ", end="")
 
-print(solution := solve(discs + ((11, 0),)))
-assert solution == (85 if EXAMPLE else 3208099)
+
+def part_2():
+    return None
+
+
+print(solution := part_2())
+#assert solution == (if EXAMPLE else)
