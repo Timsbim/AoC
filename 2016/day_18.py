0a72cfea9775180797f072488cb3e9607a10ac32
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-08 10:59:10 +0000

    Update year 2016 day 17 full solution

--- 2016/day_18.py
+++ 2016/day_18.py
@@ -1,55 +1,36 @@
 # --------------------------------------------------------------------------- #
-#    Day 17                                                                   #
+#    Day 18                                                                   #
 # --------------------------------------------------------------------------- #
-from hashlib import md5
+from pprint import pprint
 
 
-DAY = 17
-EXAMPLE = False
+DAY = 18
+EXAMPLE = True
 
 # --------------------------------------------------------------------------- #
 #    Preparation                                                              #
 # --------------------------------------------------------------------------- #
 print("Day", DAY)
 
+file_name = f"2016/input/day_{DAY:0>2}"
+if EXAMPLE:
+    file_name += "_example"
+file_name += ".txt"
+
 # --------------------------------------------------------------------------- #
 #    Reading input                                                            #
 # --------------------------------------------------------------------------- #
 
-BASE = md5(("kglvqrro" if EXAMPLE else "hhhxzeay").encode())
+with open(file_name, "r") as file:
+    pass
+if EXAMPLE:
+    #pprint()
+    pass
 
 # --------------------------------------------------------------------------- #
 #    Helper                                                                   #
 # --------------------------------------------------------------------------- #
 
-OPEN = {"b", "c", "d", "e", "f"}
-DELTA = (-1, 0), (1, 0), (0, -1), (0, 1)
-MOVE = "UDLR"
-MOVEB = "U".encode(), "D".encode(), "L".encode(), "R".encode()
-
-
-def solve(shortest=True):
-    max_path, stack = 0, [(0, 0, "", BASE)]
-    while stack:
-        stack_new = []
-        for r, c, path, h in stack:
-            for i, char in enumerate(h.hexdigest()[:4]):
-                if char not in OPEN:
-                    continue
-                dr, dc = DELTA[i]
-                if 0 <= (r1 := r + dr) < 4 and 0 <= (c1 := c + dc) < 4:
-                    path1 = path + MOVE[i]
-                    if r1 == 3 and c1 == 3:
-                        if shortest:
-                            return path1
-                        else:
-                            max_path = len(path1)
-                            continue
-                    h1 = h.copy()
-                    h1.update(MOVEB[i])
-                    stack_new.append((r1, c1, path1, h1))
-        stack = stack_new
-    return max_path
 
 
 # --------------------------------------------------------------------------- #
@@ -57,13 +38,23 @@ def solve(shortest=True):
 # --------------------------------------------------------------------------- #
 print("Part 1: ", end="")
 
-print(solution := solve())
-assert solution == ("DDUDRLRRUDRD" if EXAMPLE else "DDRUDLRRRD")
+
+def part_1():
+    return None
+
+
+print(solution := part_1())
+#assert solution == (if EXAMPLE else)
 
 # --------------------------------------------------------------------------- #
 #    Part 2                                                                   #
 # --------------------------------------------------------------------------- #
 print("Part 2: ", end="")
 
-print(solution := solve(False))
-assert solution == (492 if EXAMPLE else 398)
+
+def part_2():
+    return None
+
+
+print(solution := part_2())
+#assert solution == (if EXAMPLE else)
