b709c548bf6f732ad5847fe1d5abe22eb2f8099a
Dr. Thomas Jacobi <62063372+Timsbim@users.noreply.github.com>
2024-12-10 06:50:17 +0000

    Update refactoring year 2024 day 10

--- 2024/day_10.py
+++ 2024/day_10.py
@@ -1,9 +1,6 @@
 # --------------------------------------------------------------------------- #
 #    Day 10                                                                   #
 # --------------------------------------------------------------------------- #
-from pprint import pprint
-
-
 DAY = 10
 EXAMPLE = False
 
@@ -22,38 +19,32 @@ file_name += ".txt"
 # --------------------------------------------------------------------------- #
 
 with open(file_name, "r") as file:
-    topos = tuple(tuple(map(int, line.rstrip())) for line in file)
-if EXAMPLE:
-    pprint(topos)
+    topos = {
+        (r, c): n
+        for r, line in enumerate(file)
+        for c, n in enumerate(map(int, line.rstrip()))
+    }
 
 # --------------------------------------------------------------------------- #
 #    Helpers                                                                  #
 # --------------------------------------------------------------------------- #
-ROWS, COLS = len(topos), len(topos[0])
-
-
-def moves(r, c):
-    for dr, dc in (0, 1), (1, 0), (0, -1), (-1, 0):
-        r1, c1 = r + dr, c + dc
-        if 0 <= r1 < ROWS and 0 <= c1 < COLS:
-            yield r1, c1
 
 
 def hike(topos):
     score = count = 0
-    for r0, row in enumerate(topos):
-        for c0, n in enumerate(row):
-            if n: continue
-            heads = [(r0, c0)]
-            for n in range(1, 10):
-                heads_new = []
-                for r, c in heads:
-                    for r1, c1 in moves(r, c):
-                        if topos[r1][c1] == n:
-                            heads_new.append((r1, c1))
-                heads = heads_new
-            score += len(set(heads))
-            count += len(heads)
+    for (r0, c0), n in topos.items():
+        if n: continue
+        heads = [(r0, c0)]
+        for n in range(1, 10):
+            heads_new = []
+            for r, c in heads:
+                for dr, dc in (0, 1), (1, 0), (0, -1), (-1, 0):
+                    r1, c1 = r + dr, c + dc
+                    if topos.get((r1, c1), -1) == n:
+                        heads_new.append((r1, c1))
+            heads = heads_new
+        score += len(set(heads))
+        count += len(heads)
     return score, count
 
 
